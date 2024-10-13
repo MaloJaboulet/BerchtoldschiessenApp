e9aa177ecbfda191fe0ab9c96fabb4bf60a74cc5
Malo Jaboulet <jaboumal@students.zhaw.ch>
2024-10-13 16:13:09 +0200

    remove not needed classes

--- src/main/java/com/jaboumal/gui/MainGui.java
+++ src/main/java/com/jaboumal/gui/MainGui.java
@@ -1,13 +1,6 @@
 package com.jaboumal.gui;
 
 public class MainGui {
-    public static void main(String[] args) {
-
-        GuiFrame gui = new GuiFrame();
-
-        gui.createFrame();
-    }
-
     public void createGui(){
         GuiFrame gui = new GuiFrame();
 
